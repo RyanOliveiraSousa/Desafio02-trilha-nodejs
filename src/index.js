af252eab6cf40f104cbdb93f55c27466ff067936
Ryan <ryansousa.9810@gmail.com>
2021-07-14 10:08:42 -0300

    commit depois de resolver o desafio 2

--- src/index.js
+++ src/index.js
@@ -11,19 +11,117 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user){
+
+    return response.status(404).json("Mensagem do erro")
+
+  }
+
+  request.user = user
+
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request
+
+  if(user.pro){
+
+    return next()
+  }
+
+  if(user.todos.length >= 10){
+
+    return response.status(403).json({error: " exceed free limit"})
+  }else{
+
+    return next()
+  }
+  
+
+
+    
+  
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers
+  const {id} = request.params
+
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user){
+
+    return response.status(404).json({error: "username not found"})
+
+  }
+
+  if(!validate(id)){
+
+    return response.status(400).json({error: "is not a uuid"})
+
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+
+    return response.status(404).json({error: "id doesnt exist"})
+
+  }
+  if(!user || !todo){
+
+    return response.status(400).json({error: "user or todo doest exist"})
+  }
+
+ 
+    request.user = user
+    request.todo = todo
+
+    return next()
+  
+
+  
+ 
+
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params
+
+  const user = users.find((user) => user.id === id)
+
+  if(!user){
+
+    return response.status(404).json({error:"Mensagem do erro"})
+
+  }
+
+  request.user = user
+
+  return next()
+
+
+
 }
+
+
+
+
+
+
+
+
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
